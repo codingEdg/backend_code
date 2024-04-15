621fe3cb4b369b8ac318313918db55a7325e6b4e
Mehtab Ali <mohd2mehtab@gmail.com>
2024-04-15 23:47:47 +0530

    commit unstaged changes

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,22 +1,32 @@
-import { Router } from 'express'
-import { registerUser } from '../controllers/user.controller.js'
-import { upload } from '../middlewares/multer.middleware.js'
+import { Router } from "express";
+import {
+  loggedOutUser,
+  loginUser,
+  registerUser,
+} from "../controllers/user.controller.js";
+import { upload } from "../middlewares/multer.middleware.js";
+import { verifyJWT } from "../middlewares/auth.middleware.js";
 
-const router = Router()
+const router = Router();
 
 // this is where we define the route where uses should register with his credentials
 router.route("/register").post(
-    upload.fields([
-        {
-            name: "avatar",
-            maxCount: 1
-        },
-        {
-            name: "coverImage",
-            maxCount: 1
-        }
-    ]),
-    registerUser
-)
+  upload.fields([
+    {
+      name: "avatar",
+      maxCount: 1,
+    },
+    {
+      name: "coverImage",
+      maxCount: 1,
+    },
+  ]),
+  registerUser
+);
 
-export default router
+router.route("/login").post(loginUser);
+
+// secure routes
+router.route("/logout").post(verifyJWT, loggedOutUser);
+
+export default router;
